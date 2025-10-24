b6a59da2c4742d5e878ce7f2271928f64d2d4b55
azmal21 <mahammadazmal21@gmail.com>
2025-10-24 12:08:31 +0530

    Refactor User model to use ES6 import/export syntax

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,13 +1,10 @@
-// models/User.js
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
-  email:    { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  bio:      { type: String, default: "" }
+  bio: { type: String, default: "" }
 }, { timestamps: true });
 
-const User = mongoose.model('User', userSchema);
-export default User;
-
+export default mongoose.model("User", userSchema);
