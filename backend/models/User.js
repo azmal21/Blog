9685ac5c0f0a75672a1ce24ded4a5799bae82070
azmal21 <mahammadazmal21@gmail.com>
2025-10-24 11:28:42 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,4 +8,6 @@ const userSchema = new mongoose.Schema({
   bio:      { type: String, default: "" }
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+export default User;
+
