166986e26a8b10599ba731ea012b9cd98ee87432
azmal21 <mahammadazmal21@gmail.com>
2025-10-24 23:32:41 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,6 +4,8 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL || "http://localhost:5000/api",
 });
 
+console.log("API baseURL:", import.meta.env.VITE_APP_API_URL);
+
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
@@ -11,3 +13,4 @@ api.interceptors.request.use(config => {
 });
 
 export default api;
+
