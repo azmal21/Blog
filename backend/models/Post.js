d5a838a5f9892ba2d5c0d2ec29f4f7ceb1a38c22
azmal21 <mahammadazmal21@gmail.com>
2025-10-24 12:07:25 +0530

    Update Post.js

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
-    title: { type: String, required: true},
-    subtitle: { type: String },
-    content: { type: String, required: true},
-    authorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-    category: { type: String },
-    tags: [String],
-    image: { type: String },
-    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    views: { type: Number, default: 0}
-}, { timestamps: true});
+  title: { type: String, required: true },
+  subtitle: { type: String },
+  content: { type: String, required: true },
+  authorId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  category: { type: String },
+  tags: [String],
+  image: { type: String },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  views: { type: Number, default: 0 }
+}, { timestamps: true });
 
-const Post = mongoose.model('Post', postSchema);
-export default Post;
+export default mongoose.model("Post", postSchema);
