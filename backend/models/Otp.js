b4670dc582ffa788b61f9f31c7d02f732f002565
azmal21 <mahammadazmal21@gmail.com>
2025-10-24 12:00:54 +0530

    Update Otp.js

--- backend/models/Otp.js
+++ backend/models/Otp.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const otpSchema = new mongoose.Schema({
     email: { type: String, required: true },
     otp: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, expires: 300 }
+    createdAt: { type: Date, default: Date.now, expires: 300 } // OTP expires in 5 minutes
 });
 
-module.exports = mongoose.model("Otp", otpSchema);
+export default mongoose.model("Otp", otpSchema);
